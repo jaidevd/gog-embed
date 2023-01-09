499f332eba4a72f09c1511e30f4322de807f8116
deshpande.jaidev@gmail.com <deshpande.jaidev@gmail.com>
2023-01-09 08:53:23 +0530

    Naive captions and their templates.
    The current ones cover nearly 75% of the questionnaire.

--- template_coverage.py
+++ template_coverage.py
@@ -5,22 +5,23 @@ import yaml
 
 
 def match_template(s, tmpls):
-    matches = [re.search(pat, s) for pat in tmpls]
-    matches = [m.groupdict() if m else {} for m in matches]
-    return max(zip(tmpls, matches), key=lambda x: len(x[1]))
+    matches = [(tmpl["id"], re.search(tmpl["regex"], s)) for tmpl in tmpls]
+    matches = [(t, m.groupdict()) if m else (t, {}) for t, m in matches]
+    return max(matches, key=lambda x: len(x[1]))
 
 
 if __name__ == "__main__":
-    with open("data/qa_ns_2.json", "r") as fin:
-        qa = json.load(fin)
     with open("qa_templates.yaml", "r") as fin:
         tmpls = yaml.safe_load(fin)
-    tmpls = tmpls["retrieval"] + tmpls["reasoning"]
+    with open("data/qa_ns_1.json", "r") as fin:
+        qa = json.load(fin)
+
     res = Parallel(n_jobs=-1, verbose=2)(
         delayed(match_template)(k["question_string"], tmpls) for k in qa
     )
     res = [
-        {'question_id': q['question_id'], 'regex': k[0], 'matches': k[1]} for q, k in zip(qa, res)
+        {"question_id": q["question_id"], "regex": k[0], "matches": k[1]}
+        for q, k in zip(qa, res)
     ]
-    with open('data/matches_2.json', 'w') as fout:
+    with open("data/matches_1.json", "w") as fout:
         json.dump(res, fout, indent=2)
